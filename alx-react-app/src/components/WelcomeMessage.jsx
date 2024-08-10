6b93f35ad4a7c351180ee1c4959eb2fc4cd0dcf2
Cedrick Bienvenue <cedrick13bienvenue@gmail.com>
2024-08-10 03:38:02 +0200

    Added Welcome.jsx file

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,8 +1,7 @@
-import WelcomeMessage from './WelcomeMessage'
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone, I am learning React at ALX</h1>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>I am learning about JSX!</p>
         </div>
     );
