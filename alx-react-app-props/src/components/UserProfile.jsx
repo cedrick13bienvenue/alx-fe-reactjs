6871f39006379c0a7240385330349323ee8b2562
Cedrick Bienvenue <cedrick13bienvenue@gmail.com>
2024-08-16 17:11:04 +0200

    added Refactoring Task files

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,17 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  export default UserProfile
+import { useContext } from "react";
+import UserContext from "../components/UserContext";
+
+const UserProfile = () => {
+  // Consume the context data
+  const { name, age, bio } = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>Name: {name}</h2>
+      <p>Age: {age} years</p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
